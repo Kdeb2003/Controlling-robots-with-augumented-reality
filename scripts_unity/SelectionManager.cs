b0267c82c2d1c8ebc3df867f13cdbd259d770a4a
kushal.deb@utn.de <kushal.deb@utn.de>
2026-02-06 22:56:44 +0100

    Upload New File

---
+++ scripts_unity/SelectionManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class SelectionManager : MonoBehaviour
+{
+    public static SelectionManager I; void Awake(){ I=this; }
+    public Placeable current;
+    public void Select(Placeable p){ current=p; }
+    public void Clear(){ if(current) current=null; }
+}
